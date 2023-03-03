89553d91ab343e48ea776d9ff27802f474338b75
Jakub <sorok028@umn.edu>
2023-03-03 15:42:00 -0600

    css to homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='h-screen bg-slate-700'>
-            <h1 className='text-white py-10 text-3xl'>
-                Welcome to Sportify
-            </h1>
+        <div className='bg-slate-700 sm:h-screen'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center px-4 h-full md:flex-row'>
+                <div className='max-w-sm flex-col space-y-20 border-y-2'>
+                    <div className='my-5'>
+                        <h1 className='text-white text-4xl mb-5'>
+                            Welcome to Sportify
+                        </h1>
+                        <p className='text-white'>
+                            Your go to app for sports leagues around the country and world.  Join communities of other rabid supporters and join the discussion in your favorite league.  From the Nba to the Premiere League, find your crowd and get in on the action.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
