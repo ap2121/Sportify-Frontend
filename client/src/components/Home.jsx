efa0a57b186787d04217907817c7dd38af6d5dc3
Jakub <sorok028@umn.edu>
2023-03-03 16:01:57 -0600

    home page background changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
+import coverPhoto from '../assets/CoverPhoto.jpg';
 
 const Home = () => {
-    return (
-        <div className='bg-slate-700 sm:h-screen'>
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center px-4 h-full md:flex-row'>
-                <div className='max-w-sm flex-col space-y-20 border-y-2'>
-                    <div className='my-5'>
-                        <h1 className='text-white text-4xl mb-5'>
-                            Welcome to Sportify
-                        </h1>
-                        <p className='text-white'>
-                            Your go to app for sports leagues around the country and world.  Join communities of other rabid supporters and join the discussion in your favorite league.  From the Nba to the Premiere League, find your crowd and get in on the action.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div className="sm:h-screen bg-cover" style={{backgroundImage: `url(${coverPhoto})`}}>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center px-4 h-full md:flex-row'>
+        <div className='max-w-sm flex-col border-y-2'>
+          <div className='my-5'>
+            <h1 className='text-white text-6xl mb-5'>
+              Welcome to Sportify
+            </h1>
+            <p className='text-white'>
+              Your go to app for sports leagues around the country and world. Join communities of other rabid supporters and join the discussion in your favorite league. From the Nba to the Premiere League, find your crowd and get in on the action.
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-export default Home
+export default Home;
