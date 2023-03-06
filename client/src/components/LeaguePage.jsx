c1a09c191af29c6e29616707a9813ecc10e5c85e
Jakub <sorok028@umn.edu>
2023-03-06 15:09:18 -0600

    added map to leaguepage

--- client/src/components/LeaguePage.jsx
+++ client/src/components/LeaguePage.jsx
@@ -2,31 +2,48 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const LeaguePage = () => {
 
     const [selectedLeague, setSelectedLeague] = useState()
+    const [posts, setPosts] = useState()
 
     let { id } = useParams()
-    
-    useEffect(() => {
-    const getSportbyId = async () => {
-        const response = await axios.get(`http://localhost:3001/api/posts/posts-by-sport/${id}`)
-        setSelectedLeague(response)
-        console.log(selectedLeague);
-    }
-    getSportbyId()
-}, [id])
 
-  return (
-    <div className='h-screen bg-slate-700'>
-        <div>
-            {/* {selectedLeague.Sport.leagueName} */}
+
+
+    useEffect(() => {
+        const getSportbyId = async () => {
+            const response = await axios.get(`http://localhost:3001/api/posts/posts-by-sport/${id}`)
+            setPosts(response.data)
+            setSelectedLeague(response.data[0])
+        }
+        getSportbyId()
+    }, [id])
+
+    return selectedLeague && (
+        <div className='h-screen bg-slate-700'>
+            <div className='flex flex-col'>
+                <img src={selectedLeague.Sport.image} alt={selectedLeague.Sport.image} />
+                <h1 className='text-'>{selectedLeague.Sport.leagueName}</h1>
+                <h2>{selectedLeague.Sport.description}</h2>
+            </div>
+            <div>
+                {posts.map((post) => (
+                    <Link key={post.id} to={`/postDetails/${id}`}>
+                        <div className='text-white my-20'>
+                            Hello
+                        </div>
+
+
+                    </Link>
+                ))}
+            </div>
+            {/* Posts by Sport */}
+
         </div>
-        {/* Sport Details */}
-        {/* Posts by Sport */}
-    </div>
-  )
+    )
 }
 
 export default LeaguePage
