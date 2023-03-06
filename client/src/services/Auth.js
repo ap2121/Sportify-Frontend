3befae5acfc82eb186e9eb8dad084fe9a0d33b29
Arthur Pate <arthurpate42@gmail.com>
2023-03-06 09:21:19 -0800

    Added api for connection to backend

---
+++ client/src/services/Auth.js
@@ -0,0 +1,28 @@
+import Client from "./api";
+
+export const signInUser = async(data) => {
+    try {
+        const res = await Client.post('/api/auth/login', data)
+        localStorage.setItem('token', res.data.token)
+        return res.data.user
+    } catch(error) {
+        throw error
+    }
+}
+
+export const RegisterUser = async (data) => {
+    try {
+        const res = await Client.post('api/auth/register', data)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const CheckSession = async () => {
+    try {
+        const res = await Client.get('/api/auth/session')
+    } catch(error) {
+        throw error
+    }
+}
