225a1d35f8879c6e4e318709bf8f63b384dd99c8
Jakub <sorok028@umn.edu>
2023-03-06 11:20:06 -0600

    added sport call

--- client/src/components/LeaguePage.jsx
+++ client/src/components/LeaguePage.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const LeaguePage = () => {
 
     const [selectedLeague, setSelectedLeague] = useState()
+
+    let { id } = useParams()
     
     useEffect(() => {
     const getSportbyId = async () => {
-        const response = await axios.get(`http://localhost:3001/api/sports/all-sports/`)
+        const response = await axios.get(`http://localhost:3001/api/sports/${id}`)
         setSelectedLeague(response.data)
         console.log(response);
     }
     getSportbyId()
-}, [])
+}, [id])
 
   return (
     <div className='h-screen bg-slate-700'>
-        Team
+        {/* Sport Details */}
+        {/* Posts by Sport */}
     </div>
   )
 }
