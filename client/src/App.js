cd2d7b3217859aa9edc285e25ddd9b7a8ef93926
Jakub <sorok028@umn.edu>
2023-03-06 10:13:42 -0600

    added navbar updates.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Login from './components/Login'
 import { Route, Routes } from 'react-router-dom'
+import LeaguePage from './components/LeaguePage';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path='/' element={<Home />}/>
             <Route path='/about' element={<About />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/leaguePage/:id' element={<LeaguePage />} />
           </Routes>
           </main>
       </div>
