a63b1a443ebe27dc72f4277e56cf9d941b861ea6
Jakub <sorok028@umn.edu>
2023-03-06 14:36:35 -0600

    minor changes

--- client/src/components/LeaguePage.jsx
+++ client/src/components/LeaguePage.jsx
@@ -11,15 +11,18 @@ const LeaguePage = () => {
     
     useEffect(() => {
     const getSportbyId = async () => {
-        const response = await axios.get(`http://localhost:3001/api/sports/${id}`)
-        setSelectedLeague(response.data)
-        console.log(response);
+        const response = await axios.get(`http://localhost:3001/api/posts/posts-by-sport/${id}`)
+        setSelectedLeague(response)
+        console.log(selectedLeague);
     }
     getSportbyId()
 }, [id])
 
   return (
     <div className='h-screen bg-slate-700'>
+        <div>
+            {/* {selectedLeague.Sport.leagueName} */}
+        </div>
         {/* Sport Details */}
         {/* Posts by Sport */}
     </div>
