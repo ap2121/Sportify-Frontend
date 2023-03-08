6d4244b0530e7a06c1d55f78a29e89895f803238
Arthur Pate <arthurpate42@gmail.com>
2023-03-08 10:03:25 -0800

    before pull

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -30,7 +30,7 @@ const Login = ({ setUser }) => {
           <input type='password' placeholder='Password' onChange={handleChange} name='password' value={formValues.password} className='my-4 bg-transparent border-b-2 text-white outline-none' required />
 
           <div className='flex flex-col text-white my-20'>
-            <button class="mb-5 text-black bg-white hover:bg-gray-200 focus:outline-none transition duration-300 ease-in-out">Login</button>
+            <button className="mb-5 text-black bg-white hover:bg-gray-200 focus:outline-none transition duration-300 ease-in-out">Login</button>
 
             <Link to='/register'>
               <p>Dont have an account?</p><button className=''>Sign up here</button>
